f3773a5463c9208b16388d3c5a441a181a9aeecf
Isaac Owusu Akowuah <93617534+ioakowuah1@users.noreply.github.com>
2021-11-09 23:04:15 +0000

    Add files via upload

---
+++ Dog.js
@@ -0,0 +1,20 @@
+//variable for my age
+var myAge = 40;
+// earlyYears
+let earlyYears = 2;
+// multiply earlyYears by 10.5
+earlyYears *=10.5;
+//subtract 2 from myAge an set to new variable laterYears
+var laterYears = myAge - 2;
+// dog years by multiplying laterYears by 4
+laterYears *=4;
+console.log(earlyYears + ',' + laterYears);
+// add earlyYears and laterYears
+var myAgeInDogYears = earlyYears + laterYears;
+//save my name and change to lowercase
+var myName = "isaac".toLowerCase();
+// print out for ouput
+console.log(`My name is NAME. I am HUMAN AGE years old in human years which is DOG AGE years old in dog years`);
+// replacing some strings with the variables to print out
+console.log(`My name is ${myName}. I am ${myAge}, and ${myAgeInDogYears}.`);
+
